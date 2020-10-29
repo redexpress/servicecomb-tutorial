b7811a30da0b1dcc3916cc79ec7a1b38d57d6ba4
gaofeng <gaofeng@oxhainan.org>
2020-10-29 15:20:34 +0800

    update

--- hello/src/main/java/io/github/redexpress/hello/HelloEndpoint.java
+++ hello/src/main/java/io/github/redexpress/hello/HelloEndpoint.java
@@ -1,11 +1,12 @@
 package io.github.redexpress.hello;
 
 import org.apache.servicecomb.provider.rest.common.RestSchema;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestSchema(schemaId = "hello")
-@RequestMapping(path = "/")
+@RequestMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
 public class HelloEndpoint {
     @GetMapping(path = "/hello")
     public String hello() {
---
+++ simpleweb/simpleweb-math/src/main/java/io/github/redexpress/math/Hello.java
@@ -0,0 +1,2 @@
+package io.github.redexpress.math;public class Hello {
+}
