7a254a4f05aeac61ffed05577a07b3138f2b93a3
gaofeng <gaofeng@oxhainan.org>
2020-10-30 08:31:41 +0800

    add dubbo

---
+++ simpleweb/mydubbo/src/main/java/io/github/redexpress/mydubbo/MydubboApplication.java
@@ -0,0 +1,13 @@
+package io.github.redexpress.mydubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydubboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydubboApplication.class, args);
+	}
+
+}
---
+++ simpleweb/mydubbo/src/test/java/io/github/redexpress/mydubbo/MydubboApplicationTest.java
@@ -0,0 +1,10 @@
+package io.github.redexpress.mydubbo;
+import org.junit.*;
+
+public class MydubboApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
